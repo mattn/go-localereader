6338b4c69507fb1f9edba3db33882a8e9ab9bfa8
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2022-06-18 03:06:34 +0900

    add UTF8

--- localereader_test.go
+++ localereader_test.go
@@ -26,3 +26,15 @@ func TestDecoder(t *testing.T) {
 		t.Fatalf("want %q, but got %q", want, got)
 	}
 }
+
+func TestUTF8(t *testing.T) {
+	b := []byte{0x01, 0xFF, 0x82, 0xA0, 0x82, 0xA4, 0x82, 0xA6, 0x82, 0xA8, 0x82}
+	got, err := UTF8(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	want := "\x01\xFFあいうえお\x82"
+	if reflect.DeepEqual(got, want) {
+		t.Fatalf("want %q, but got %q", want, got)
+	}
+}
